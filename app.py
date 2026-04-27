a0fd374ff4b2969d5d7c1dbe33cf142eba90509b
Anushka <anushkasgaikwad204@gmail.com>
2026-04-27 12:42:01 -0400

    Text changed in Power

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ html = """
   <button name="op" value="mul">Multiply</button>
   <button name="op" value="div">Divide</button>
   <button name="op" value="mod">Remainder</button>
-  <button name="op" value="pow">Power</button>
+  <button name="op" value="pow">Power (Raised to)</button>
 </form>
 
 {% if result is not none %}

5a19b44db63afe1cbdfcf56d41009d48deb0cf32
Anushka <anushkasgaikwad204@gmail.com>
2026-04-27 12:51:39 -0400

    Meger Resolved

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ html = """
   <button name="op" value="sub">Subtract</button>
   <button name="op" value="mul">Multiply</button>
   <button name="op" value="div">Divide</button>
+  <button name="op" value="mod">Modulus</button>
   <button name="op" value="mod">Remainder</button>
   <button name="op" value="pow">Power (Raised to)</button>
 </form>
